d3b8f4183e099721f7b791dc6c0c52aa226a5484
Colin Vitols <vitolscn@mail.uc.edu>
2021-09-09 14:40:13 -0400

    updating javadoc

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,12 +3,29 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Handle the start endpoint
+ * @return
+ *
+ */
 @Controller
 public class PlantPlacesController {
+	
 	
 	@RequestMapping("/start")
 	public String start() {
 		
 		return "start";
 	}
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 *
+	 */
+
+		@RequestMapping("/")
+		public String index() {
+			
+			return "index";
+		}
 }
